51c41fafc013b6840604e896f32fba1ef3d1b38f
sosotheso <64717767+sosotheso@users.noreply.github.com>
2022-05-20 23:19:27 +0200

    Euler3 solution

---
+++ Euler3.py
@@ -0,0 +1,20 @@
+import math
+
+def primefactors(n):
+    # even number divisible
+    while n % 2 == 0:
+        print(2),
+        n = n / 2
+
+    # n became odd
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+
+        while n % i == 0:
+            print(i)
+            n = n / i
+
+    if n > 2:
+        print(n)
+
+
+primefactors(600851475143)
